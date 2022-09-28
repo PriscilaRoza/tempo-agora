6a58a691193773ccc6d92da00e549ed1d1850aef
Priscila Roza <priscilarozasilva@gmail.com>
2022-09-27 23:04:16 -0300

    api do clima integrado e funcionando

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-//1fc343c6dcad8ad280ecf2025f2077ef
+//
+//variáveis e seleção de elementos
+const apiKey = "1fc343c6dcad8ad280ecf2025f2077ef";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+//funções
+//acessa api
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+
+}
+
+//exibe dados da api
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+    
+    cityElement.innerHTML = data.name;
+    tempElement.innerHTML = parseInt(data.main.temp);
+    descElement.innerHTML = data.weather[0].description;
+    weatherIconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute('src', apiCountryURL + data.sys.country);
+    humidityElement.innerHTML = `${data.main.humidity}%`;
+    windElement.innerHTML = `${data.wind.speed}km/h`
+
+
+
+}
+
+
+
+//eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+})
